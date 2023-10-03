31df36bd279a939dba3c7ddb629f806c224680fd
Debiddo <dzheng@terpmail.umd.edu>
2023-10-03 02:34:50 -0400

    final code cleaning

--- main.py
+++ main.py
@@ -4,33 +4,40 @@ import pandas as pd
 # Flask is a website object
 app = Flask(__name__)
 
-station=pd.read_csv("data_small/stations.txt",skiprows=17)
-station=station[["STAID","STANAME                                 "]]
+station = pd.read_csv("data_small/stations.txt", skiprows=17)
+station = station[["STAID", "STANAME                                 "]]
+
+
 @app.route("/")
 def home():
     # Flask will look at templates folder by default
     return render_template("home.html", data=station.to_html())
+
+
 @app.route("/api/v1/<station>/<date>")
-def about(station,date):
-    filename = "data_small/TG_STAID"+str(station).zfill(6)+".txt"
+def about(station, date):
+    filename = "data_small/TG_STAID" + str(station).zfill(6) + ".txt"
     df = pd.read_csv(filename, skiprows=20, parse_dates=["    DATE"])
-    temperature = df.loc[df["    DATE"]==date]["   TG"].squeeze()/10
-    return {"station":station,"date":date,"temperature":temperature}
+    temperature = df.loc[df["    DATE"] == date]["   TG"].squeeze() / 10
+    return {"station": station, "date": date, "temperature": temperature}
+
 
 @app.route("/api/v1/<station>")
 def all_data(station):
-    filename = "data_small/TG_STAID"+str(station).zfill(6)+".txt"
+    filename = "data_small/TG_STAID" + str(station).zfill(6) + ".txt"
     df = pd.read_csv(filename, skiprows=20, parse_dates=["    DATE"])
     result = df.to_dict(orient="records")
     return result
 
+
 @app.route("/api/v1/yearly/<station>/<year>")
-def yearly(station,year):
-    filename = "data_small/TG_STAID"+str(station).zfill(6)+".txt"
+def yearly(station, year):
+    filename = "data_small/TG_STAID" + str(station).zfill(6) + ".txt"
     df = pd.read_csv(filename, skiprows=20)
     df["    DATE"] = df["    DATE"].astype(str)
     result = df.loc[df["    DATE"].str.startswith(str(year))].to_dict(orient="records")
     return result
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     app.run(debug=True, port=5001)
